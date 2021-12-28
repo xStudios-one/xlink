4bdbfd3f8e0eb7838e0ce8ca05df090025d23163
xJulek <xjulek123@gmail.com>
2021-12-28 19:07:46 +0100

    Add license info in main.js

--- modules/main.js
+++ modules/main.js
@@ -1,3 +1,16 @@
+/*
+            ______     __                      __  __                     
+           /      \   |  \                    |  \|  \                    
+ __    __ |  $$$$$$\ _| $$_    __    __   ____| $$ \$$  ______    _______ 
+|  \  /  \| $$___\$$|   $$ \  |  \  |  \ /      $$|  \ /      \  /       \
+ \$$\/  $$ \$$    \  \$$$$$$  | $$  | $$|  $$$$$$$| $$|  $$$$$$\|  $$$$$$$
+  >$$  $$  _\$$$$$$\  | $$ __ | $$  | $$| $$  | $$| $$| $$  | $$ \$$    \ 
+ /  $$$$\ |  \__| $$  | $$|  \| $$__/ $$| $$__| $$| $$| $$__/ $$ _\$$$$$$\
+|  $$ \$$\ \$$    $$   \$$  $$ \$$    $$ \$$    $$| $$ \$$    $$|       $$
+ \$$   \$$  \$$$$$$     \$$$$   \$$$$$$   \$$$$$$$ \$$  \$$$$$$  \$$$$$$$ 
+ xLink © 2021 by xstudios.one is licensed under CC BY-ND 4.0 
+*/
+
 const signale = require('signale');
 const { Signale } = require('signale');
 const loader = signale.scope('loader');
@@ -17,7 +30,22 @@ function main() {
     starter.fatal('[2/5] linker.js not found');
   }, 8000);
   setTimeout(() => {
-    starter.info('Please note that this is not accually loading anything yet.');
+    starter.await('[3/5] Loading comms.js');
   }, 10000);
+  setTimeout(() => {
+    starter.fatal('[3/5] comms.js not found');
+  }, 14000);
+  setTimeout(() => {
+    comms.time('communication with device');
+  }, 15000);
+  setTimeout(() => {
+    comms.timeEnd('communication with device');
+    comms.error(
+      'connection could not be established with device on port: /dvc/581r/comms'
+    );
+  }, 20246);
+  setTimeout(() => {
+    starter.info('Please note that this is not accually loading anything yet.');
+  }, 16000);
 }
 main();
