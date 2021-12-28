82fc0a60fc5b1ab42d63f27798c38e41c66c0495
xJulek <xjulek123@gmail.com>
2021-12-28 18:07:44 +0100

    start signing commits, add dummy loader and app in main.js

--- modules/main.js
+++ modules/main.js
@@ -1,4 +1,23 @@
+const signale = require('signale');
+const { Signale } = require('signale');
+const loader = signale.scope('loader');
+const linker = signale.scope('linker');
+const comms = signale.scope('communicator');
+require('./logodisplay');
 function main() {
-  console.trace('error no app installed');
+  const starter = new Signale({ interactive: true, scope: 'loader' });
+  starter.await('[1/5] Loading main.js');
+  setTimeout(() => {
+    starter.success('[1/5] Loading succcess!');
+  }, 2000);
+  setTimeout(() => {
+    starter.await('[2/5] Loading linker.js');
+  }, 4000);
+  setTimeout(() => {
+    starter.fatal('[2/5] linker.js not found');
+  }, 8000);
+  setTimeout(() => {
+    starter.info('Please note that this is not accually loading anything yet.');
+  }, 10000);
 }
 main();
